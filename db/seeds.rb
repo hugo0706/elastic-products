cca8961a2fe90dedc5d00c9c804e1a3981641684
hugo0706 <hugo.garciac@edu.uah.es>
2024-08-18 17:36:24 +0200

    Created seed script to populate db with dataset

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'csv'
+
+AMAZON_SEED_PATH="./db/seeds/amazon/"
+
+def parse_number(number)
+  return 0.0 unless number
+  number.gsub("₹", "").gsub(',', '').to_f
+end
+
+Dir.each_child(AMAZON_SEED_PATH) do |file|
+  headers = nil
+
+  File.open(AMAZON_SEED_PATH + file, 'r') do |file|
+    headers = CSV.parse_line(file.readline)
+    headers = Hash[headers.map.with_index { |element, index| [ element.to_sym, index ] }]
+  end
+
+  CSV.foreach(AMAZON_SEED_PATH + file).with_index do |row, index|
+    next if index == 0
+    begin
+      ActiveRecord::Base.transaction do
+        data = {
+          name: row[headers[:name]],
+          main_category:  row[headers[:main_category]],
+          sub_category:   row[headers[:sub_category]],
+          image:          row[headers[:image]],
+          link:           row[headers[:link]],
+          ratings:        parse_number(row[headers[:ratings]]),
+          no_of_ratings:  parse_number(row[headers[:no_of_ratings]]),
+          discount_price: parse_number(row[headers[:discount_price]]),
+          actual_price:   parse_number(row[headers[:actual_price]])
+        }
+        Product.create!(**data)
+      end
+    rescue StandardError => e
+      puts "Error while seeding db with #{file} \n #{e.message}"
+    end
+  end
+end
